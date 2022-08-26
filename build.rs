f83051fe628670e182ddc658dfaa5e830d88b380
Kraken-Mare <michkos2005@gmail.com>
2022-08-26 12:11:09 +0200

    windows fix

--- build.rs
+++ build.rs
@@ -80,16 +80,17 @@ mod tests{
 }
 //TODO:extend this function(use symlinks?) to allow for multipile versions of mono to be used.
 fn copy_win_dlls(){
+    use std::path::Path;
     //check if "mono-2.0-sgen.dll" present, and copy it if not
-    let msgen_taraget_path = "mono-2.0-sgen.dll";
+    let msgen_taraget_path = Path::new("mono-2.0-sgen.dll");
     if !msgen_taraget_path.exists(){
-        let msgen_source_path = "C:\\Program Files\\Mono\\bin\\mono-2.0-sgen.dll";
+        let msgen_source_path =  Path::new("C:\\Program Files\\Mono\\bin\\mono-2.0-sgen.dll");
         std::fs::copy(msgen_source_path,msgen_taraget_path).expect("Could not copy `mono-2.0-sgen.dll` file. Is mono propely installed on your system?");
     }
     //check if mscorlib.dll file is present and if not copy
-    let mcl_target_path = "..\\lib\\mono\\4.5\\mscorlib.dll"
+    let mcl_target_path = Path::new("..\\lib\\mono\\4.5\\mscorlib.dll");
     if !mcl_target_path.exists(){
-        let mcl_source_path = "C:\\Program Files\\Mono\\lib\\mono\\4.5\\mscorlib.dll";
+        let mcl_source_path = Path::new("C:\\Program Files\\Mono\\lib\\mono\\4.5\\mscorlib.dll");
         std::fs::copy(mcl_source_path,mcl_target_path).expect("Could not copy `mscorlib.dll` file. Is mono propely installed on your system?");
     }
 }
