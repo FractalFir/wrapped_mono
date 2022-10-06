0f40489ee5f0914ecf70a22a425e14f741fa4a33
FractalFir <fractalfirdev@gmail.com>
2022-10-06 18:26:08 +0200

    Changed tests.

--- src/testing/gc.rs
+++ src/testing/gc.rs
@@ -65,8 +65,8 @@ rusty_fork_test! {
         let dom = jit::init("dom",None);
         let mut results:Vec<Array<1,i32>> = Vec::with_capacity(4000);
         println!("Preparing to create test arrays!");
-        // Having more Arrays fills up the nursery, and causes problems with garbage collection
-        for i in 0..1700{
+        // Having more temporary Arrays fills up the nursery, and causes problems with garbage collection(can't unlock a thread)
+        for i in 0..17000{
             let mut obj:Array<1,i32> = Array::new(&dom,&[i/50 as usize]);
             for j in 0..10{
                 obj = Array::new(&dom,&[i/50 as usize]);
