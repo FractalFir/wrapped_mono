a2acf42355a688f1a337a384e8b0195a02617b1d
FractalFir <FractalFir@github.com>
2022-10-10 23:07:30 +0200

    Minor documentation changes and fixes for Image type.

--- src/image.rs
+++ src/image.rs
@@ -1,5 +1,5 @@
 use crate::binds::MonoImage;
-/// Safe representation [`MonoImage`], part of [`MonoAssembly`] holding CLI code.
+/// Safe representation [`MonoImage`], the part of [`MonoAssembly`] holding CLI code.
 #[derive(Copy,Clone)]
 pub struct Image{
     img_ptr:*mut MonoImage,
@@ -7,13 +7,13 @@ pub struct Image{
 use std::ffi::CString;
 use crate::metadata::{MetadataTableInfo,MetadataTableKind,MetadataToken};
 impl Image{
-    ///Gets metadata table from an image.
+    /// Gets metadata table from an image.
     pub fn get_table_info(&self,kind:MetadataTableKind)->MetadataTableInfo{
         unsafe{MetadataTableInfo::from_ptr(
             crate::binds::mono_image_get_table_info(self.img_ptr,kind as i32)
         ,kind)}
     }
-    ///Gets string from metadata. *index* must be within the string heap.
+    /// Gets string from metadata string heap. *index* must be within the string heap.
     pub fn metadata_string_heap(&self,index:MetadataToken)->String{
         let cstr = unsafe{CString::from_raw(
             crate::binds::mono_metadata_string_heap(self.img_ptr,index) as *mut i8)
@@ -22,17 +22,17 @@ impl Image{
         let _ = cstr.into_raw();
         res
     }
-    ///Creates the value of [`Image`] type from a [`MonoImage`].
+    /// Creates the value of [`Image`] type from a [`MonoImage`].
     /// # Safety
-    /// *ptr* must be a valid  image pointer.
+    /// *ptr* must be a pointer to a valid [`MonoImage`].
     pub unsafe fn from_ptr(ptr:*mut crate::binds::MonoImage)->Self{
         Self{img_ptr:ptr}
     }
-    //Returns internal pointer
+    /// Returns internal pointer to [`MonoImage`] this [`Image`] represents.
     pub fn get_ptr(&self)->*mut MonoImage{
         self.img_ptr
     }
-    ///Initialize all global varaibles in image.
+    /// Initializes all global variables in image(static members of classes).
     pub fn init(&self){
         unsafe{crate::binds:: mono_image_init(self.img_ptr)};
     }
@@ -44,9 +44,9 @@ impl Image{
         let _ = cstr.into_raw();
         name
     }
-    ///Closes this image. 
+    /// Closes this image, unloading it from memory.
     /// # Safety
-    /// All referneces to types within image will be made invalid after this call.
+    /// All references to types within image will be made invalid after this call.
     pub fn close(&mut self){
         unsafe{crate::binds::mono_image_close(self.img_ptr)};
         crate::hold(&self);
